396c4527b7abe8319f7707ea06fcdb9aeb9f7473
Your Name <youremail@example.com>
2025-08-29 23:57:20 +0300

    обновление README.md

--- app/main.go
+++ app/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"app/bot"
 	"app/bot/config"
-	"app/bot/storage"
 	"log"
 )
 
 func main() {
-	defer storage.CloseConnection()
 	config.LoadAll()
 
 	if err := bot.Start(); err != nil {
